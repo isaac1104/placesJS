73840f42e4a7c6ecf1cab774c57e1aa2052134fc
isaac1104 <isaac-kwon@hotmail.com>
2019-05-13 12:38:30 +0900

    lazyload Landing & Home with loading spinner

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,16 +1,20 @@
-import React, { Component } from 'react';
+import React, { Component, Suspense, lazy } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import Landing from './Landing/Landing';
-import Home from './Home/Home';
+import Spinner from './Spinner/Spinner';
+
+const Landing = lazy(() => import('./Landing/Landing'));
+const Home = lazy(() => import('./Home/Home'));
 
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
-        <Switch>
-          <Route exact path='/' component={Landing} />
-          <Route exact path='/home' component={Home} />
-        </Switch>
+        <Suspense fallback={<Spinner />}>
+          <Switch>
+            <Route exact path='/' component={Landing} />
+            <Route exact path='/home' component={Home} />
+          </Switch>
+        </Suspense>
       </BrowserRouter>
     );
   }
