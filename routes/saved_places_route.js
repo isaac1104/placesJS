144c15ac5588831c29b19ea1436ac4916726e911
isaac1104 <isaac-kwon@hotmail.com>
2019-05-13 21:15:27 +0900

    protect api routes if not logged in

--- routes/saved_places_route.js
+++ routes/saved_places_route.js
@@ -1,13 +1,14 @@
 const mongoose = require('mongoose');
 const SavedPlace = mongoose.model('SavedPlace');
+const requireAuth = require('../middlewares/requireAuth');
 
 module.exports = app => {
-  app.get('/api/saved_places', async (req, res) => {
+  app.get('/api/saved_places', requireAuth, async (req, res) => {
     const savedPlace = await SavedPlace.find({ _user: req.user.id });
     res.status(200).send(savedPlace);
   });
 
-  app.post('/api/saved_places', async (req, res) => {
+  app.post('/api/saved_places', requireAuth, async (req, res) => {
     const { title, description, latitude, longitude } = req.body;
     const currentPlace = await SavedPlace.find({ latitude, latitude });
     if (currentPlace.length !== 0) {
