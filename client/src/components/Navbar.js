91970cda330311dac50c6be4a8f9f960ab97d16d
isaac1104 <isaac-kwon@hotmail.com>
2019-05-13 13:05:46 +0900

    show navbar when current user data is not empty

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -5,22 +5,35 @@ import { connect } from 'react-redux';
 const { Header } = Layout;
 
 class Navbar extends Component {
+  renderNavbar() {
+    const { data } = this.props.currentUser;
+
+    if (data) {
+      return (
+        <Header>
+          <div className='logo' />
+          <Menu
+            theme='dark'
+            mode='horizontal'
+            defaultSelectedKeys={['2']}
+            style={{ lineHeight: '64px' }}
+          >
+            <Menu.Item key='1'>nav 1</Menu.Item>
+            <Menu.Item key='2'>nav 2</Menu.Item>
+            <Menu.Item key='3'>nav 3</Menu.Item>
+          </Menu>
+        </Header>
+      );
+    }
+
+    return null;
+  }
+
   render() {
-    console.log(this.props.currentUser);
     return (
-      <Header>
-        <div className='logo' />
-        <Menu
-          theme='dark'
-          mode='horizontal'
-          defaultSelectedKeys={['2']}
-          style={{ lineHeight: '64px' }}
-        >
-          <Menu.Item key='1'>nav 1</Menu.Item>
-          <Menu.Item key='2'>nav 2</Menu.Item>
-          <Menu.Item key='3'>nav 3</Menu.Item>
-        </Menu>
-      </Header>
+      <>
+        {this.renderNavbar()}
+      </>
     );
   }
 }
