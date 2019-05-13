fbc73a0e9ad2bca1163a360d98a0fa0e96376041
isaac1104 <isaac-kwon@hotmail.com>
2019-05-13 13:01:44 +0900

    fetch current user data when app component mounts

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,5 +1,7 @@
 import React, { Component, Suspense, lazy } from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { fetchCurrentUserData } from '../actions';
 import Navbar from './Navbar';
 import Spinner from './Spinner/Spinner';
 
@@ -7,13 +9,28 @@ const Landing = lazy(() => import('./Landing/Landing'));
 const Home = lazy(() => import('./Home/Home'));
 
 class App extends Component {
+  componentDidMount() {
+    this.props.fetchCurrentUserData();
+  }
+
   render() {
     return (
       <BrowserRouter>
         <Navbar />
         <Suspense fallback={<Spinner />}>
           <Switch>
-            <Route exact path='/' component={Landing} />
+            <Route
+              exact
+              path='/'
+              render={() => {
+                const { data } = this.props.currentUser;
+                if (data) {
+                  return <Redirect to='/home' />;
+                } else {
+                  return <Landing />;
+                }
+              }}
+            />
             <Route exact path='/home' component={Home} />
           </Switch>
         </Suspense>
@@ -22,4 +39,10 @@ class App extends Component {
   }
 }
 
-export default App;
+const mapStateToProps = ({ currentUser }) => {
+  return {
+    currentUser
+  };
+};
+
+export default connect(mapStateToProps, { fetchCurrentUserData })(App);
