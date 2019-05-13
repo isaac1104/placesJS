d479461cf79f4f6ce595f4e4a82c5ba198cdcbf5
isaac1104 <isaac-kwon@hotmail.com>
2019-05-13 12:26:08 +0900

    render map on Home.js

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
+import Map from 'pigeon-maps';
+import Marker from 'pigeon-marker';
+import classes from './Home.module.css';
 
 class Home extends Component {
   render() {
     return (
-      <div>
-        <h1>Welcome to home route</h1>
+      <div className={classes.HomeContainer}>
+        <Map center={[50.879, 4.6997]} zoom={12}>
+          <Marker anchor={[50.874, 4.6947]} payload={1} onClick={({ event, anchor, payload }) => {}} />
+        </Map>
       </div>
     );
   }
