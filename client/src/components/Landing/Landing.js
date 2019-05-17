7570958ed948546c948690ce65f76f21643be90b
isaac1104 <isaac-kwon@hotmail.com>
2019-05-17 10:52:33 +0900

    add "fade" animation on landing page with react-reveal

--- client/src/components/Landing/Landing.js
+++ client/src/components/Landing/Landing.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Fade from 'react-reveal/Fade';
 import { Button, Typography } from 'antd';
 import classes from './Landing.module.css';
 
@@ -7,19 +8,23 @@ const { Title } = Typography;
 const Landing = () => (
   <div className={classes.LandingContainer}>
     <div>
-      <Title className={classes.LandingTitle}>
-        <span role='img' aria-label='earth-emoji'>🌎</span> Welcome to PlacesJS
-      </Title>
-      <Button
-        block
-        icon='google'
-        type='primary'
-        size='large'
-        shape='round'
-        href='/auth/google'
-      >
-        Sign-in with Google
-      </Button>
+      <Fade top>
+        <Title className={classes.LandingTitle}>
+          <span role='img' aria-label='earth-emoji'>🌎</span> Welcome to PlacesJS
+        </Title>
+      </Fade>
+      <Fade bottom>
+        <Button
+          block
+          icon='google'
+          type='primary'
+          size='large'
+          shape='round'
+          href='/auth/google'
+        >
+          Sign-in with Google
+        </Button>
+      </Fade>
     </div>
   </div>
 );
